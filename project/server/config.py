4635904fc3d5b9e849e048c9e43da0c2b07b2e30
Dhanunjaya Panchireddy <dhanunjaya.panchireddy@gmail.com>
2017-12-22 11:06:54 +0530

    init

--- project/server/config.py
+++ project/server/config.py
@@ -3,10 +3,11 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+TEST_DB = "test.db"
 
 class BaseConfig(object):
     """Base configuration."""
-    SECRET_KEY = 'my_precious'
+    SECRET_KEY = 'b\xfa\xe4\x94<\xd4wm\x84\x0f7\xe6)H\x0c\xe48\xedE\xcc^\xcaN?'
     DEBUG = False
     BCRYPT_LOG_ROUNDS = 13
     WTF_CSRF_ENABLED = True
@@ -20,8 +21,7 @@ class DevelopmentConfig(BaseConfig):
     DEBUG = True
     BCRYPT_LOG_ROUNDS = 4
     WTF_CSRF_ENABLED = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///{0}'.format(
-        os.path.join(basedir, 'dev.db'))
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///{0}'.format(os.path.join(basedir, 'dev.db'))
     DEBUG_TB_ENABLED = True
 
 
@@ -31,14 +31,14 @@ class TestingConfig(BaseConfig):
     TESTING = True
     BCRYPT_LOG_ROUNDS = 4
     WTF_CSRF_ENABLED = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///{0}'.format(os.path.join(basedir, TEST_DB))
     DEBUG_TB_ENABLED = False
     PRESERVE_CONTEXT_ON_EXCEPTION = False
 
 
 class ProductionConfig(BaseConfig):
     """Production configuration."""
-    SECRET_KEY = 'my_precious'
+    SECRET_KEY = 'b\xfa\xe4\x94<\xd4wm\x84\x0f7\xe6)H\x0c\xe48\xedE\xcc^\xcaN?'
     DEBUG = False
     SQLALCHEMY_DATABASE_URI = 'postgresql://localhost/example'
     DEBUG_TB_ENABLED = False
